1cb05dce925c21d54d37f69604a34d00f87202e7
Alexander Murphy <amurphy345@gmail.com>
2018-09-03 13:28:34 -0600

    adding pan gesture recognizer

--- PanGestureExamples/ViewController.swift
+++ PanGestureExamples/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let square = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        square.translatesAutoresizingMaskIntoConstraints = false
+        square.backgroundColor = .green
+        square.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(squareDidPan(_:))))
+        
+        view.addSubview(square)
+        
+        NSLayoutConstraint.activate([
+            NSLayoutConstraint(item: square, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 150),
+            NSLayoutConstraint(item: square, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 150),
+            NSLayoutConstraint(item: square, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: 100),
+            NSLayoutConstraint(item: square, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0)
+            ])
     }
 
 
+    @objc private func squareDidPan(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        
+        square.center = CGPoint(x: square.center.x + translation.x, y: square.center.y + translation.y)
+        sender.setTranslation(.zero, in: view)
+    }
 }
 
